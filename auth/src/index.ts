93641ed134c6f241cd676e7f48f5f0fda3f76116
stilyng94 <oseipaulkofi@gmail.com>
2021-03-08 21:53:09 +0000

    added startup ms to auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 import { DatabaseConnectionError } from "@lotus_yng_dev_tickets/common";
 
 const main = async () => {
+  console.log("Starting auth service");
   try {
     await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
